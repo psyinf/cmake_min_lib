4dd7b25624dde5bf5056c4bab8b163856992bf2b
psy_inf <sebastian@smesserschmidt.de>
2023-10-19 11:48:29 +0200

    Minimal library

---
+++ src/minLib.cpp
@@ -0,0 +1,15 @@
+#include "minLib.h"
+#include <iostream>
+
+minLib::ClassWithWarnings::ClassWithWarnings()
+{
+}
+
+minLib::ClassWithWarnings::~ClassWithWarnings()
+{
+}
+
+void minLib::ClassWithWarnings::doSomething()
+{
+    puts("Done something");
+}
---
+++ src/minLib.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace minLib{
+ 
+class ClassWithWarnings
+{
+public:
+    ClassWithWarnings();
+    ~ClassWithWarnings();
+    void doSomething();
+};
+
+ 
+} //namespace myLib
