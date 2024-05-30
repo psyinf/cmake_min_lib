10b528bb39e7ba4aa9322668fbe63d2d92d1c826
psy_inf <sebastian@smesserschmidt.de>
2024-05-30 09:21:04 +0200

    Added minimal cmake-presets

--- include/minLib.h
+++ include/minLib.h
@@ -1,17 +1,17 @@
 #pragma once
 
-namespace minLib{
- 
-class ClassWithWarnings
-{
-public:
-    ClassWithWarnings() = default;  
-    ~ClassWithWarnings() = default;
-    void doSomething();
-};
+namespace minLib {
 
-inline float doSomethingElse(double x) {
-    return 1e23 + 0.00000000000001;
-}
- 
+    class ClassWithWarnings
+    {
+    public:
+        ClassWithWarnings() = default;
+        ~ClassWithWarnings() = default;
+        void doSomething();
+    };
+
+    inline float doSomethingElse([[maybe_unused]] double x) {
+        return 1e23 + 0.00000000000001;
+    }
+
 } //namespace myLib
